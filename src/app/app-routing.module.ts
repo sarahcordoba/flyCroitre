edf609b33527d0a4ba4328438ac462ff5df75d8c
sarahcordoba <sarahcordobasalgado@gmail.com>
2021-11-24 14:35:42 -0500

    Enrutamientos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+  },{
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },{
+    path: 'admin',
+    loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+  },{
+    path: 'aeropuertos',
+    loadChildren: () => import('./modulos/aeropuertos/aeropueros.module').then(m => m.AeropuerosModule)
+  },{
+    path: 'rutas',
+    loadChildren: () => import('./modulos/rutas/rutas.module').then(m => m.RutasModule)
+  },{
+    path: 'vuelos',
+    loadChildren: () => import('./modulos/vuelos/vuelos.module').then(m => m.VuelosModule)
+  },{
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/index'
+  },{
+    path: 'error',
+    component: ErrorComponent,
+  },{
+    path: '**',
+    redirectTo: '/error'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CambiarClaveComponent } from './cambiar-clave/cambiar-clave.component';
+import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
+import { LoginComponent } from './login/login.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: "logout",
+    component: CerrarSesionComponent,
+  },{
+    path: 'cambiar-clave',
+    component: CambiarClaveComponent,
+  },{
+    path: 'recuperar-clave',
+    component: RecuperarClaveComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
